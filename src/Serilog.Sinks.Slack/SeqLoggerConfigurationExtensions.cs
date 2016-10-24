270fe2751225a0f240cd59dfcb4e9ecf62e152b8
Alastair Pitts <alastair@rateitapp.com>
2016-10-24 17:34:27 +1100

    Added the ability to include logevent properties - These will be set to slack as fields in the initial attachment - Added the ability to tidy any stack trace of async calls, condensing the output to slack.

--- src/Serilog.Sinks.Slack/SeqLoggerConfigurationExtensions.cs
+++ src/Serilog.Sinks.Slack/SeqLoggerConfigurationExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using Serilog.Configuration;
 using Serilog.Core;
 using Serilog.Events;
@@ -21,18 +22,14 @@ namespace Serilog.Sinks.Slack
         /// </summary>
         /// <param name="loggerSinkConfiguration">Instance of <see cref="LoggerSinkConfiguration"/> object.</param>
         /// <param name="webhookUrl">Slack team post URI.</param>
+        /// <param name="logEventProperties"></param>
+        /// <param name="tidyStackTraces"></param>
         /// <param name="customChannel">Name of Slack channel to which message should be posted.</param>
         /// <param name="customUsername">User name that will be displayed as a name of the message sender.</param>
         /// <param name="customIcon">Icon that will be used as a sender avatar.</param>
         /// <param name="restrictedToMinimumLevel"><see cref="LogEventLevel"/> value that specifies minimum logging level that will be allowed to be logged.</param>
         /// <returns>Instance of <see cref="LoggerConfiguration"/> object.</returns>
-        public static LoggerConfiguration Slack(
-            this LoggerSinkConfiguration loggerSinkConfiguration,
-            string webhookUrl,
-            string customChannel = null,
-            string customUsername = null,
-            string customIcon = null,
-            LogEventLevel restrictedToMinimumLevel = LevelAlias.Minimum)
+        public static LoggerConfiguration Slack(this LoggerSinkConfiguration loggerSinkConfiguration, string webhookUrl,  string customChannel = null, string customUsername = null, string customIcon = null, IEnumerable<string> logEventProperties = null, bool tidyStackTraces = false, LogEventLevel restrictedToMinimumLevel = LevelAlias.Minimum)
         {
             if (loggerSinkConfiguration == null)
                 throw new ArgumentNullException(nameof(loggerSinkConfiguration));
@@ -40,7 +37,7 @@ namespace Serilog.Sinks.Slack
             if (string.IsNullOrWhiteSpace(webhookUrl))
                 throw new ArgumentNullException(nameof(webhookUrl));
 
-            ILogEventSink sink = new SlackSink(webhookUrl, customChannel, customUsername, customIcon);
+            ILogEventSink sink = new SlackSink(webhookUrl, customChannel, customUsername, customIcon, logEventProperties, tidyStackTraces);
 
             return loggerSinkConfiguration.Sink(sink, restrictedToMinimumLevel);
         }
--- src/Serilog.Sinks.Slack/SlackSink.cs
+++ src/Serilog.Sinks.Slack/SlackSink.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Dynamic;
 using System.Globalization;
 using System.IO;
+using System.Linq;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
@@ -28,10 +29,12 @@ namespace Serilog.Sinks.Slack
             {LogEventLevel.Fatal, "#d9534f"}
         };
 
-		private readonly string _webhookUrl;
-		private readonly string _customChannel;
+        private readonly string _webhookUrl;
+        private readonly string _customChannel;
         private readonly string _customUserName;
         private readonly string _customIcon;
+        private readonly IEnumerable<string> _logEventProperties;
+        private readonly bool _tidyStackTraces;
         private readonly JsonSerializer _serializer;
 
         /// <summary>
@@ -41,17 +44,23 @@ namespace Serilog.Sinks.Slack
         /// <param name="customChannel">Name of Slack channel to which message should be posted.</param>
         /// <param name="customUserName">User name that will be displayed as a name of the message sender.</param>
         /// <param name="customIcon">Icon that will be used as a sender avatar.</param>
+        /// <param name="logEventProperties">A collection of custom log event properties to include as attachments</param>
+        /// <param name="tidyStackTraces"></param>
         public SlackSink(
             string webhookUrl,
             string customChannel = null,
             string customUserName = null,
-            string customIcon = null)
+            string customIcon = null,
+            IEnumerable<string> logEventProperties = null,
+            bool tidyStackTraces = false)
             : base(50, TimeSpan.FromSeconds(5))
         {
             _webhookUrl = webhookUrl;
             _customChannel = customChannel;
             _customUserName = customUserName;
             _customIcon = customIcon;
+            _logEventProperties = logEventProperties ?? Enumerable.Empty<string>();
+            _tidyStackTraces = tidyStackTraces;
             _serializer = new JsonSerializer();
         }
 
@@ -64,7 +73,7 @@ namespace Serilog.Sinks.Slack
         {
             foreach (var logEvent in events)
             {
-                var message = CreateMessage(logEvent, _customChannel, _customUserName, _customIcon);
+                var message = CreateMessage(logEvent, _customChannel, _customUserName, _customIcon, _logEventProperties, _tidyStackTraces);
 
                 var json = SerializeMessage(message);
 
@@ -85,19 +94,19 @@ namespace Serilog.Sinks.Slack
             return sb.ToString();
         }
 
-        private static dynamic CreateMessage(LogEvent logEvent, string channel, string username, string emoji)
+        private static dynamic CreateMessage(LogEvent logEvent, string channel, string username, string emoji, IEnumerable<string> logEventProperties, bool tidyStackTraces)
         {
             dynamic message = new ExpandoObject();
             message.text = logEvent.RenderMessage();
             message.channel = string.IsNullOrWhiteSpace(channel) ? string.Empty : channel;
             message.username = string.IsNullOrWhiteSpace(username) ? string.Empty : username;
             message.icon_emoji = string.IsNullOrWhiteSpace(emoji) ? string.Empty : emoji;
-            message.attachments = CreateAttachments(logEvent);
+            message.attachments = CreateAttachments(logEvent, logEventProperties, tidyStackTraces);
 
             return message;
         }
 
-        private static List<dynamic> CreateAttachments(LogEvent logEvent)
+        private static List<dynamic> CreateAttachments(LogEvent logEvent, IEnumerable<string> logEventProperties, bool tidyStackTraces)
         {
             var attachments = new List<dynamic>
             {
@@ -113,33 +122,46 @@ namespace Serilog.Sinks.Slack
                 }
             };
 
+            //write any custom log event properties we have been asked to include
+            var initAttachment = attachments[0];
+            foreach (var propertyName in logEventProperties)
+            {
+                LogEventPropertyValue value;
+                if (logEvent.Properties.TryGetValue(propertyName, out value))
+                    initAttachment.fields.Add(new { title = propertyName, value = value.ToString() });
+            }
+
             if (logEvent.Exception == null)
                 return attachments;
 
-            var stackTrace = logEvent.Exception.StackTrace.Split('\n');
-            var filteredStackTrace = new List<string>();
-            var lastWasAsync = false;
-
-            foreach (var line in stackTrace)
+            var finalStackTrace = logEvent.Exception.StackTrace;
+            if (tidyStackTraces)
             {
-                if (line.TrimStart().StartsWith("at System.Runtime.CompilerServices.")
-                    )
+                var stackTrace = logEvent.Exception.StackTrace.Split('\n');
+                var filteredStackTrace = new List<string>();
+                var lastWasAsync = false;
+
+                foreach (var line in stackTrace)
                 {
-                    lastWasAsync = true;
-                    continue;
+                    if (line.TrimStart().StartsWith("at System.Runtime.CompilerServices.")
+                        )
+                    {
+                        lastWasAsync = true;
+                        continue;
+                    }
+
+                    if (lastWasAsync)
+                    {
+                        filteredStackTrace.Add(" -- (async)\r");
+                    }
+
+                    filteredStackTrace.Add(line);
+
+                    lastWasAsync = false;
                 }
 
-                if (lastWasAsync)
-                {
-                    filteredStackTrace.Add(" -- (async)\r");
-                }
-
-                filteredStackTrace.Add(line);
-
-                lastWasAsync = false;
+                finalStackTrace = string.Join("\n", filteredStackTrace.ToArray());
             }
-
-            var finalStackTrace = string.Join("\n", filteredStackTrace.ToArray());
 
             attachments.Add(new
             {
